8640677eafd560b1fceb8a455d96be2481af90c1
maxmargolin <margolinmax@gmail.com>
2017-09-01 19:28:45 +0300

    Working Example!!! excited

---
+++ a.js
@@ -0,0 +1,10 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+if(location.href=="https://www.youtube.com/watch?v=weeI1G46q0o")
+    location.href="https://youtu.be/weeI1G46q0o?t=29s";
+    //document.body.style.backgroundColor="Gray" ;
+   // location.href
+
+})();
