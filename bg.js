3b397e8940351a5efb4e23a4a52746a8778be797
maxmargolin <margolinmax@gmail.com>
2017-09-02 13:29:11 +0300

    fixes,sample data

--- bg.js
+++ bg.js
@@ -6,21 +6,38 @@ window.addEventListener("load", Process); // one-time late postprocessing
 
 
 
+
 function process() {
-// this is the code which will be injected into a given page...
+    var db = [
+        ["weeI1G46q0o", 29],
+        ["YQHsXMglC9A", 77],
+        ["iGk5fR-t5AU", 65],
+        ["kOkQ4T5WO9E", 15],
+        ["CTFtOOh47oo", 21],
+        ["dPI-mRFEIH0", 23],
+        ["DK_0jXPuIr0", 35],//
+        ["2vjPBrBU-TM", 10],
+        ["uxpDa-c-4Mc", 19],
+        ["34Na4j8AVgA", 42],
+        ["EgqUJOudrcM", 29],
+        ["UprcpdwuwCg", 18],
+        ["k0BWlvnBmIE", 43],
+        ["uuwfgXD8qV8", s9]
+    ];
 
-var curl=location.href;
-var timed = (curl.includes("\&t\=") || curl.includes("\?t\="));
-if(!timed) {
-    if (curl.includes("weeI1G46q0o"))
-    {
-        location.href += "&t=29";
 
+
+    var curl=location.href;
+    var timed = (curl.includes("\&t\=") || curl.includes("\?t\="));
+    if(!timed) {
+        var keep=true;
+        for (i = 0; i < db.length && keep; i++){
+        if (curl.includes("v\="+db[i][0])) {
+            location.href += "&t=" + db[i][1];
+            keep = false;
+        }
 }
-    //if(location.href=="https://www.youtube.com/watch?v=weeI1G46q0o")
 
-    //  else if(location.href=="https://www.youtube.com/watch?v=YQHsXMglC9A")
-    //        location.href="https://youtu.be/YQHsXMglC9A?t=1m17s";
 }
 }
 
