addd4382f87707b82309009a2ba25b840be2f8e1
maxmargolin <margolinmax@gmail.com>
2017-09-30 18:48:34 +0300

    support for mid and end video skips

--- bg.js
+++ bg.js
@@ -1,28 +1,28 @@
 chrome.storage.local.get("version", function(result) {
-  currentDBVersion = 6; //change to force update
-  if (!(result["version"] === currentDBVersion)) {
-    var db = get_data(currentDBVersion);
+        currentDBVersion = 8; //change to force update
+        if (!(result["version"][0] === currentDBVersion)) {
+                var db = get_data(currentDBVersion);
 
-    //add to local storage
-    for (var i = 0; i < db.length; i++) {
-      var obj = {};
-      obj[db[i][0]] = db[i].slice(1);
-      chrome.storage.local.set(obj);
-    }
+                //add to local storage
+                for (var i = 0; i < db.length; i++) {
+                        var obj = {};
+                        obj[db[i][0]] = db[i].slice(1);
+                        chrome.storage.local.set(obj);
+                }
 
 
-  }
+        }
 });
 
 
 
 //begin counting total
 chrome.storage.sync.get("totalTime", function(time) {
-  if (time["totalTime"] == undefined) {
-    var obj = {};
-    obj["totalTime"] = 0;
-    chrome.storage.sync.set(obj);
-  }
+        if (time["totalTime"] == undefined) {
+                var obj = {};
+                obj["totalTime"] = 0;
+                chrome.storage.sync.set(obj);
+        }
 });
 
 
@@ -38,16 +38,16 @@ window.addEventListener("load", process); // one-time late postprocessing
 
 //popup is requesting id
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.greeting == "hello") {
-      var curl = location.href;
-      var vID = curl.match(/v\=(.{11})/);
-      sendResponse({
-        farewell: vID[1]
-      });
+        function(request, sender, sendResponse) {
+                if (request.greeting == "hello") {
+                        var curl = location.href;
+                        var vID = curl.match(/v\=(.{11})/);
+                        sendResponse({
+                                farewell: vID[1]
+                        });
 
-    }
-  });
+                }
+        });
 
 
 //skip part of video
@@ -55,49 +55,50 @@ function process() {
 
 
 
-  var curl = location.href; //current url
-  var vID = curl.match(/v\=(.{11})/); //regex for ID
+        var curl = location.href; //current url
+        var vID = curl.match(/v\=(.{11})/); //regex for ID
 
-  var timed = (curl.includes("\&t\=") || curl.includes("\?t\="));
-  if (!timed) { //only if video is not timed
+        var timed = (curl.includes("\&t\=") || curl.includes("\?t\="));
+        if (!timed) { //only if video is not timed
 
-    chrome.storage.local.get("on", function(result) {
-      if (result["on"]) { //sync storage beforelocal
-        chrome.storage.sync.get(vID[1], function(result) {
-          if (!(result[vID[1]] === undefined || result[vID[1]] == 0)) {
-            window.location.replace(location.href + "&t=" + result[vID[1]]); //change url
+                chrome.storage.local.get("on", function(result) {
+                        if (result["on"]) { //sync storage beforelocal
+                                chrome.storage.sync.get(vID[1], function(result) {
+                                        if (!(result[vID[1]] === undefined || result[vID[1]] == 0)) {
+                                                window.location.replace(location.href + "&t=" + result[vID[1]]); //change url
 
-            chrome.storage.sync.get("totalTime", function(time) {
-              var newTime = {};
-              newTime["totalTime"] = parseInt(time["totalTime"]) + parseInt(result[vID[1]]);
-              chrome.storage.sync.set(newTime);
-            });
-          } else {
-            chrome.storage.local.get(vID[1], function(result) {
-              if (!(result[vID[1]][0] === undefined || result[vID[1]][0] == 0)) {
-                window.location.replace(location.href + "&t=" + result[vID[1]][0]); //change url
+                                                TotalTimeUpdate(result[vID[1]]);
+
+                                        } else {
+                                                chrome.storage.local.get(vID[1], function(result) {
+                                                        if (!(result[vID[1]][0] === undefined || result[vID[1]][0] == 0)) {
+                                                                window.location.replace(location.href + "&t=" + result[vID[1]][0]); //change url
 
 
-                chrome.storage.sync.get("totalTime", function(time) {
-                  var newTime = {};
-                  newTime["totalTime"] = parseInt(time["totalTime"]) + parseInt(result[vID[1]][0]);
-                  chrome.storage.sync.set(newTime);
+                                                                TotalTimeUpdate(result[vID[1]][0]);
+
+                                                        }
+                                                });
+                                        }
+                                });
+
+
+                        }
+                        $(".date style-scope ytd-video-secondary-info-renderer").text("Hello world!");
+
                 });
 
-              }
-            });
-          }
+
+
+
+
+        }
+}
+
+function TotalTimeUpdate(toAdd) {
+        chrome.storage.sync.get("totalTime", function(time) {
+                var newTime = {};
+                newTime["totalTime"] = parseInt(time["totalTime"]) + parseInt(toAdd);
+                chrome.storage.sync.set(newTime);
         });
-
-
-      }
-      $(".date style-scope ytd-video-secondary-info-renderer").text("Hello world!");
-
-    });
-
-
-
-
-
-  }
 }
--- inner.js
+++ inner.js
@@ -1,25 +1,38 @@
-setInterval(alertFunc, 1000);
+setInterval(InSkipper, 1000);
 
 
 
 
-function alertFunc() {
-  let video = document.querySelector("video.html5-main-video");
-  let currentTime = video.currentTime; // Fractional, in seconds
-  let totalDuration = video.duration;
-  //video.currentTime+=100;
-  var curl = location.href;
-  var vID = curl.match(/v\=(.{11})/);
+function InSkipper() {
+        chrome.storage.local.get("on", function(result) {
+                if (result["on"]) {
+                        let video = document.querySelector("video.html5-main-video");
+                        let currentTime = video.currentTime; // Fractional, in seconds
+                        let totalDuration = video.duration;
+                        //video.currentTime+=100;
+                        var curl = location.href;
+                        var vID = curl.match(/v\=(.{11})/);
 
 
-      chrome.storage.local.get(vID[1], function(result) {
-        for (var i = 1; i < result[vID[1]].length-1; i+=2) {
-          if (currentTime >= result[vID[1]][i] && currentTime <= (result[vID[1]][i] + 3))
-            video.currentTime = result[vID[1]][i+1];
-          }
-        if ((result[vID[1]].length%2)==0 && currentTime >= result[vID[1]][result[vID[1]].length-1] && Math.floor(currentTime<totalDuration))
-          video.currentTime = Math.ceil(totalDuration);
-      });
+                        chrome.storage.local.get(vID[1], function(result) {
+                                for (var i = 1; i < result[vID[1]].length - 1; i += 2) {
+                                        var skipTime = result[vID[1]][i];
+                                        if (inSkipRange(currentTime, skipTime)) {
+                                                video.currentTime = result[vID[1]][i + 1];
+                                                TotalTimeUpdate(result[vID[1]][i + 1] - currentTime);
+                                        }
+                                }
 
+                                if ((result[vID[1]].length % 2) == 0 && currentTime >= result[vID[1]][result[vID[1]].length - 1] && Math.floor(currentTime < totalDuration)) {
+                                        video.currentTime = Math.ceil(totalDuration);
+                                        TotalTimeUpdate(totalDuration - currentTime);
+                                }
+                        });
+                }
+        });
 
+}
+
+function inSkipRange(current_time, skip_time) {
+        return (current_time >= skip_time && current_time <= skip_time + 3);
 }
